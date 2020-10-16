d93af846140f1829eaf0a4df8e1fbc510261af84
Shaik Ghouse Basha <ShaikGhouse.Basha@geappliances.com>
2020-10-16 20:33:04 +0530

    Initial Commit

---
+++ GridSearchSwiftUI/GridSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchSwiftUIApp.swift
+//  GridSearchSwiftUI
+//
+//  Created by Shaik Ghouse Basha on 16/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
